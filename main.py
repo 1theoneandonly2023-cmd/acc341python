0cab3925815d8b79416c4cc7d22d648338169e5d
Avner Musheyev <1theoneandonly2023@gmail.com>
2026-04-27 17:46:48 -0400

    revenue for cutomers

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('python with neon DB')
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Load DATABASE_URL from your .env file
+load_dotenv()
+conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+
+# A cursor lets you send SQL commands
+cursor = conn.cursor()
+
+# --- Swap in any query from the Query Reference section ---
+cursor.execute("SELECT invoice_id, customer_id, due_date, total_amount FROM invoice WHERE status = 'Overdue';")
+
+# Fetch and print every row returned
+rows = cursor.fetchall()
+for row in rows:
+
+ print(row)
+
+# Always close when finished
+cursor.close()
+conn.close()
